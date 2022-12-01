e38667224a9f7aef4fca62b6d0ce000655895771
Kniffen <knifftech@gmail.com>
2022-12-01 08:05:49 +0100

    Day 01: part 2

--- day-01/index.test.ts
+++ day-01/index.test.ts
@@ -2,14 +2,20 @@ import path from 'path'
 import fs from 'fs'
 
 import part1 from './part1'
+import part2 from './part2'
 
 describe('day 01', function() {
+  const input = fs.readFileSync(path.resolve(__dirname, './testInput.txt'), 'utf8')
+  
   describe('part 1', function() {
     it('Should find the elf carrying the most calories', function() {
-      const input = fs.readFileSync(path.resolve(__dirname, './testInput.txt'), 'utf8')
-      const elf = part1(input)
+      expect(part1(input)).toEqual(24000)
+    })
+  })
 
-      expect(elf).toEqual(24000)
+  describe('part 2', function() {
+    it('Should find the total calories of the top 3 elves', function() {
+      expect(part2(input)).toEqual(45000)
     })
   })
 })
