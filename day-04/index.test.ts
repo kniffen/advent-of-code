be05fd94729eef777807c2dcbd1bdf63f29042fc
Kniffen <kniffen@knifftech.org>
2022-12-04 07:01:47 +0100

    Day 04: part 2

--- day-04/index.test.ts
+++ day-04/index.test.ts
@@ -2,9 +2,11 @@ import path from 'path'
 import fs from 'fs'
 
 import part1 from './part1'
+import part2 from './part2'
 
 describe('Day 04', function() {
   const input = fs.readFileSync(path.resolve(__dirname, './testInput.txt'), 'utf8')
   
   it('part 1', () => expect(part1(input)).toEqual(2))
+  it('part 2', () => expect(part2(input)).toEqual(4))
 })
