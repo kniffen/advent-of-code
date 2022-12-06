628db98b4334991022580dea662b9b2ef683ba82
Kniffen <kniffen@knifftech.org>
2022-12-06 12:39:58 +0100

    Update index.ts

--- day-06/index.ts
+++ day-06/index.ts
@@ -5,7 +5,9 @@ import getMarker from './getMarker'
 
 const input = fs.readFileSync(path.resolve(__dirname, 'input.txt'), 'utf8')
 
-console.log('Advent of Code 2022: Day 06')
-console.log('---------------------------')
-console.log(`Part 1: ${getMarker(input, 4)}`)
-console.log(`Part 2: ${getMarker(input, 14)}`)
+console.log(`
+Advent of Code 2022: Day 06
+---------------------------
+Part 1: ${getMarker(input, 4)}
+Part 2: ${getMarker(input, 14)}
+`)
