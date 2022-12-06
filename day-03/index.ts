0eb2958e766192a3fe0f8911d3613f47f095c286
Kniffen <kniffen@knifftech.org>
2022-12-06 12:14:27 +0100

    Update index.ts

--- day-03/index.ts
+++ day-03/index.ts
@@ -6,7 +6,9 @@ import part2 from './part2'
 
 const input = fs.readFileSync(path.resolve(__dirname, 'input.txt'), 'utf8')
 
-console.log('Advent of Code 2022: Day 03')
-console.log('---------------------------')
-console.log(`Part 1: ${part1(input)}`)
-console.log(`Part 2: ${part2(input)}`)
+console.log(`
+Advent of Code 2022: Day 03
+---------------------------
+Part 1: ${part1(input)}
+Part 2: ${part2(input)}
+`)
