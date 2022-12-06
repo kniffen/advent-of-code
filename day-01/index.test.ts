b818e6038b2da9abd6bfd4391525aa806afcc38a
Kniffen <kniffen@knifftech.org>
2022-12-06 12:11:11 +0100

    Update index.test.ts

--- day-01/index.test.ts
+++ day-01/index.test.ts
@@ -7,15 +7,6 @@ import part2 from './part2'
 describe('day 01', function() {
   const input = fs.readFileSync(path.resolve(__dirname, './testInput.txt'), 'utf8')
   
-  describe('part 1', function() {
-    it('Should find the elf carrying the most calories', function() {
-      expect(part1(input)).toEqual(24000)
-    })
-  })
-
-  describe('part 2', function() {
-    it('Should find the total calories of the top 3 elves', function() {
-      expect(part2(input)).toEqual(45000)
-    })
-  })
+  it('part 1', () => expect(part1(input)).toEqual(24000))
+  it('part 2', () => expect(part2(input)).toEqual(45000))
 })
