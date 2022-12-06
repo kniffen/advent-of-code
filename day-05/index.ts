0f2e6dc60bcff23476edbb89468bc42da659a2c4
Kniffen <kniffen@knifftech.org>
2022-12-06 12:39:15 +0100

    Update index.ts

--- day-05/index.ts
+++ day-05/index.ts
@@ -5,7 +5,9 @@ import getTopCrates from './getTopCrates'
 
 const input = fs.readFileSync(path.resolve(__dirname, 'input.txt'), 'utf8')
 
-console.log('Advent of Code 2022: Day 05')
-console.log('---------------------------')
-console.log(`Part 1: ${getTopCrates(input)}`)
-console.log(`Part 2: ${getTopCrates(input, true)}`)
+console.log(`
+Advent of Code 2022: Day 05
+---------------------------
+Part 1: ${getTopCrates(input)}
+Part 2: ${getTopCrates(input, true)}
+`)
